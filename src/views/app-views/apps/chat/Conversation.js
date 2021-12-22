ed2895d469702aea6c35488af655f56dd44691ee
chungit201 <vuongvanchung30092001@gmail.com>
2021-12-22 18:07:21 +0700

    update 141

--- src/views/app-views/apps/chat/Conversation.js
+++ src/views/app-views/apps/chat/Conversation.js
@@ -47,7 +47,7 @@ const Conversation = () => {
   const token = JSON.parse(sessionStorage.getItem('access_token'));
   const [socket, setSocket] = useState();
   useEffect(() => {
-    const newSocket = io.connect('https://bb-assignment-react-01.herokuapp.com', {
+    const newSocket = io.connect('https://server-my-app-0012.herokuapp.com', {
       auth: { token: token },
       transports: ['websocket'],
     });
